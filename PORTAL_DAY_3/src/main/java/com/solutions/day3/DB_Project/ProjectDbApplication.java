aa8a055a028d0ca46d830b9a33e752dc13ec3bc1
Hari Charan <106080542+HariCharanR@users.noreply.github.com>
2023-08-21 16:00:53 +0530

    Add files via upload

---
+++ PORTAL_DAY_3/src/main/java/com/solutions/day3/DB_Project/ProjectDbApplication.java
@@ -0,0 +1,13 @@
+package com.solutions.day3.DB_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDbApplication.class, args);
+	}
+
+}
---
+++ PORTAL_DAY_3/src/main/java/com/solutions/day3/DB_Project/model/Employee.java
@@ -0,0 +1,66 @@
+package com.solutions.day3.DB_Project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Employees")
+public class Employee {
+	@Id 
+	private int id;
+	
+	private String e_name;
+	
+	private int e_age;
+	
+	private long e_salary;
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(int id, String e_name, int e_age, long e_salary) {
+		super();
+		this.id = id;
+		this.e_name = e_name;
+		this.e_age = e_age;
+		this.e_salary = e_salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getE_name() {
+		return e_name;
+	}
+
+	public void setE_name(String e_name) {
+		this.e_name = e_name;
+	}
+
+	public int getE_age() {
+		return e_age;
+	}
+
+	public void setE_age(int e_age) {
+		this.e_age = e_age;
+	}
+
+	public long getE_salary() {
+		return e_salary;
+	}
+
+	public void setE_salary(long e_salary) {
+		this.e_salary = e_salary;
+	}
+	
+	
+	
+}
